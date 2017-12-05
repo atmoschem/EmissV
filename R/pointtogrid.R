4183b48a1bcad77af07dc14220d292f93681bca2
Schuch666 <underschuch@gmail.com>
2017-12-05 02:15:17 -0200

    points

--- R/pointtogrid.R
+++ R/pointtogrid.R
@@ -1,8 +1,8 @@
 #' create a point source
 #'
-#' @description under development (test)
+#' @description Transform a set of points into a grinded output
 #'
-#' @param p input point object
+#' @param p list of points
 #' @param grid grid object with the grid information
 #' @param verbose display adicional information
 #'
@@ -10,37 +10,36 @@
 #'
 #' @seealso \code{\link{newGrid}} and \code{\link{rasterToGrid}}
 #'
-pointToGrid <- function(p,grid,verbose = T){
+pointToGrid <- function(p = list(x=-24.67123,y=-47.26636),grid,verbose = T){
+  # g     <- grid with class SpatialPolygonsDataFrame
+  # spobj <- A spatial dataframe of class sp
+  x <- p$x
+  y <- p$y
+  p <- SpatialPoints(data.frame(x,y))
 
-  print("test")
+  cc <- matrix(c(grid$Lon,grid$Lat),ncol = 2,byrow = F)
 
-  return(NA)
+  x <- grid$Lon
+  y <- grid$Lat
+  p2 <- SpatialPoints(data.frame(x,y))
+  g <- SpatialGrid(p2)
+
+  # g <- sp::over(g,spobj, fn=sum)
+  # return(g)
+  return(g)
 }
 
-# olhar emis_grid do vein :
-
-# FUNÇÃO NO PACOTE VEIN
-# emis_grid <- function(spobj, g, sr, type="lines"){
-#   if ( type == "lines" ) {
-#     net <- spobj
-#     for( i in 1:ncol(net@data) ){
-#       net@data[,i] <- as.numeric(net@data[,i])
-#     }
-#     net$lkm <-  rgeos::gLength(sp::spTransform(net,CRS(sr)),byid = T)/1000
-#     netg <- raster::intersect(net,g)
-#     netg$lkm2 <-  rgeos::gLength(sp::spTransform(netg,CRS(sr)),byid = T)/1000
-#     netg@data[,1:(ncol(netg@data)-3)] <-  netg@data[,1:(ncol(netg@data)-3)] * netg$lkm2/netg$lkm
-#     dfm <- stats::aggregate(cbind(netg@data[,1:(ncol(netg@data)-3)]),
-#                             by=list(netg$id), sum, na.rm=TRUE)
-#     colnames(dfm)[1] <- "id"
-#     gg <- merge(g, dfm, by="id")
-#     # for(i in 2:ncol(gg)){
-#     #   gg@data[,i] <- gg@data[,i] * units::parse_unit("g h-1")
-#     # } # spplot does not work with units
-#     return(gg)
-#   } else if ( type == "points" ){
-#     g@data <- sp::over(g,spobj, fn=sum)
-#     #Add units
-#     return(g)
-#   }
-# }
+# x = "SpatialPoints", y = "SpatialGrid"
+# xx
+#
+# x = "SpatialPoints", y = "SpatialGridDataFrame"
+# xx
+#
+# x = "SpatialPoints", y = "SpatialPixels"
+# xx
+#
+# x = "SpatialPoints", y = "SpatialPixelsDataFrame"
+# xx
+#
+# x = "SpatialPolygons", y = "SpatialGridDataFrame"
+# xx
