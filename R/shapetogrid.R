f9c4a1545ffec4c781576cc4730b2a5e250b85dd
Schuch666 <underschuch@gmail.com>
2017-12-11 11:55:05 -0200

    alguns testes

--- R/shapetogrid.R
+++ R/shapetogrid.R
@@ -10,13 +10,64 @@
 #'
 #' @seealso \code{\link{newGrid}} and \code{\link{rasterToGrid}}
 #'
+#'
+#' @examples \dontrun{
+#' # Do not run
+#' grid  <- newGrid(paste(system.file("extdata", package = "EmissV"),"/wrfinput_d02",sep=""))
+#' d3 <- data.frame(x = as.numeric(grid$Lon),y = as.numeric(grid$Lat))
+#' d3 <- st_as_sf(d3, coords = c("x","y"))
+#' st_crs(d3) <- st_crs(4326)
+#'
+#' streets <- readRDS("D:/Downloads/streets.rds")
+#' streets <- streets[streets$highway != "residential", ]
+#'
+#' shapeToGrid(streets,d3)
+#'
+#' }
 shapeToGrid <- function(s,grid,verbose = T){
+  s        <- st_transform(s, crs = st_crs(grid))
+  # le       <- as.numeric( sf::st_length(s) )
+  # grid_L   <- st_intersection(Len, grid)
 
-  return()
+  test <- raster::raster(raster::extent(s), crs=st_crs(s)) # usar : nrows=180, ncols=360,
+  test.poly <- rasterToPolygons(test)
+  rp <- rgeos::gIntersection(s, test.poly, byid=TRUE)
+  rp <- SpatialLinesDataFrame(rp, data.frame(row.names=sapply(slot(rp, "lines"),function(x) slot(x, "ID")), ID=1:length(rp),
+                                             length=SpatialLinesLengths(rp)/1000) )
+  # Rasterize using sum of intersected lines
+  rd <- rasterize(rp, rrst, field="length", fun="sum")
+
+  return(rd)
 }
 
+# # fonte: https://gis.stackexchange.com/questions/119993/convert-line-shapefile-to-raster-value-total-length-of-lines-within-cell
+# require(rgdal)
+# require(raster)
+# require(sp)
+# require(rgeos)
+#
+# setwd("D:/TEST/RDSUM")
+# roads <- readOGR(getwd(), "TZA_roads")
+# roads <- spTransform(roads, CRS("+init=epsg:21037"))
+# rrst  <- raster(extent(roads), crs=projection(roads))
+#
+# # Intersect lines with raster "polygons" and add length to new lines segments
+# rrst.poly <- rasterToPolygons(rrst)
+# rp <- gIntersection(roads, rrst.poly, byid=TRUE)
+# rp <- SpatialLinesDataFrame(rp, data.frame(row.names=sapply(slot(rp, "lines"),
+#                                                             function(x) slot(x, "ID")), ID=1:length(rp),
+#                                            length=SpatialLinesLengths(rp)/1000) )
+#
+# # Rasterize using sum of intersected lines
+# rd.rst <- rasterize(rp, rrst, field="length", fun="sum")
+#
+# # Plot results
+# require(RColorBrewer)
+# spplot(rd.rst, scales = list(draw=TRUE), xlab="x", ylab="y",
+#        col.regions=colorRampPalette(brewer.pal(9, "YlOrRd")),
+#        sp.layout=list("sp.lines", rp),
+#        par.settings=list(fontsize=list(text=15)), at=seq(0, 1800, 200))
 # olhar emis_grid do vein :
-
 # emis_grid <- function(spobj, g, sr){
 #     net <- spobj
 #     for( i in 1:ncol(net@data) ){
@@ -35,6 +86,3 @@ shapeToGrid <- function(s,grid,verbose = T){
 #     # } # spplot does not work with units
 #     return(gg)
 #}
-# spobj:
-# g:
-# sr:
