ae563c08dbf76ab6fc17506d30bf2c3321aa75eb
Schuch666 <underschuch@gmail.com>
2017-12-18 14:27:49 -0200

    new names and plume rise function

--- NEWS.md
+++ NEWS.md
@@ -1,2 +1,5 @@
+## EmissV 0.661 (2017-12-18)
+- New function names and future work
+
 ## EmissV 0.66 (2017-12-08)
 -I'm not alone: Sergio Joint me and send me a sweet function
---
+++ R/plumerise.R
@@ -0,0 +1,21 @@
+#' Calculate plume rise from Brigs (1975) formulation.
+#'
+#' @description return plume rise
+#'
+#' @format a numeric value in (units) meters
+#'
+#' @param verbose display adicional information
+#'
+#' @export
+#'
+#' @examples \dontrun{
+#' # Do not run
+#'
+#'
+#'}
+
+plumeRise <- function(verbose = T)
+{
+  rise <- 0
+  return(rise)
+}
