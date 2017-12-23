321fd71f40b2caf1c420a8914da1bdd6d596101e
Schuch666 <underschuch@gmail.com>
2017-12-23 03:35:47 -0200

    emission works with a matrix of list of areaSource

--- R/emission.R
+++ R/emission.R
@@ -6,7 +6,7 @@
 #'
 #' @param total list of total emission
 #' @param pol pollutant name
-#' @param area list of area sources
+#' @param area list of area sources or matrix with a spatial distribution
 #' @param grid grid information
 #' @param mm pollutant molar mass
 #' @param aerosol TRUE for aerosols and FALSE (defoult) for gazes
@@ -78,22 +78,28 @@ emission <- function(total,pol,area,grid, mm = 1, aerosol = F, verbose = T){
 
   var <- total[[n]]
 
-  # get the units (in order to work with raster)
-  unidade <- total[[1]][1]/as.numeric(total[[1]][[1]])
+  if(is.raster(area)){
+    # get the units (in order to work with raster)
+    unidade <- total[[1]][1]/as.numeric(total[[1]][[1]])
 
-  for(i in 1:length(area)){
-    area[[i]] = area[[i]] * var[[i]]
+    for(i in 1:length(area)){
+      area[[i]] = area[[i]] * var[[i]]
+    }
+    area <- unname(area)
+
+    VAR_e  <- do.call(sp::merge,area)
+
+    VAR_e[is.na(VAR_e)]     <- 0
+
+    VAR_e <- rasterSource(VAR_e,grid,verbose = F)
+
+    # put the units (to back the unit)
+    VAR_e <- VAR_e * unidade
   }
-  area <- unname(area)
-
-  VAR_e  <- do.call(sp::merge,area)
-
-  VAR_e[is.na(VAR_e)]     <- 0
-
-  VAR_e <- rasterSource(VAR_e,grid,verbose = F)
-
-  # put the units (to back the unit)
-  VAR_e <- VAR_e * unidade
+  if(is.matrix(area)){
+    VAR_e               <- area * var
+    VAR_e[is.na(VAR_e)] <- 0
+  }
 
   dx <- grid$DX
   dx =  units::set_units(dx,km)
