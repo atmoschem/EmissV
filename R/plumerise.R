03acc557eb3971a7766f04bf9ba986edbc27f8ea
Schuch666 <underschuch@gmail.com>
2018-04-02 21:12:57 -0300

    plume rise test

--- R/plumerise.R
+++ R/plumerise.R
@@ -58,15 +58,25 @@ plumeRise <- function(df, imax = 1000, ermax = 1/100, Hmax = T, verbose = T)
         if(verbose)
           print(paste("convective, h/L =",h/L))
         Wd     <- 0.4 * Wstar # downdrafts mean speed
-        deltaH <- Flu / U*Wd^2
+        a      <- ( Flu / U*Wd^2 )^(3/5)
+        b      <- 2 * Hs
+        deltaH <- b
         for(i in 1:imax){
-          old <- deltaH
-          deltaH <- ( Flu / U*Wd^2 )^(3/5) * (1 + 2*Hs/deltaH)^2
-          err    <- (deltaH - old )/old
-          if(err <= ermax){
-            # print(paste(i,"iterações"))
-            i = imax
-          }
+          old    <- deltaH
+          deltaH <- a * (1 + b/old)^2
+          # deltaH <- old + 0.1 * (old^3 / 2* b) * ((1 + b/old)^2 / (2*b*(b + old)))
+          # deltaH<- old - 0.1 * (3*b + old)/(2 * old)
+          err    <- abs( (deltaH - old )/old )
+          print(paste("OLD=",old))
+          print(paste("Dh=",deltaH))
+          print(paste("err=",err))
+          print("========================================")
+          # print(paste("Dh=",deltaH))
+          # if(err <= ermax){
+          #   print(paste(i,"iterações"))
+          #   print(paste("Dh=",deltaH))
+          #   i = imax
+          # }
         }
       }else
         # neutral
@@ -105,3 +115,90 @@ plumeRise <- function(df, imax = 1000, ermax = 1/100, Hmax = T, verbose = T)
   df <- cbind(df,He)
   return(df)
 }
+# !     ******************* PLUME RISE **********************
+#
+#   !      SUBROUTINE PRISE(L,Q,QEFF,ALT,WE,UR,AFOE,HF,AFO,DH,US,NE,NR,FBE,FB,ES,pkr,tss,tff,d,vs)
+# SUBROUTINE PRISE(L,Q,QEFF,ALT,WE,UR,AFOE,AFO,DH,US,FBE,FB,ES,tss,tff,d,vs)
+# IMPLICIT REAL*8 (A-Z)
+# REAL*8 USTAR,ELLE,WSTAR,HMX,ES,tff,tss,d,vs	  !hf,
+# REAL*8 HS,US,TS,AFO,FB,GRADT,DH,ESSE,QEFF,FBE !,AA,AJ,AL,GU,PU
+# !INTEGER NE,NR,pkr
+#
+# d=9.
+# FB=9.81*((D/2.)**2.)*VS*((Tss-Tff)/Tss)
+# FBE=FB/(US*ALT*(WE**2.))
+# !      FBE=FB/(US*ALT*(WE**2.))
+# USTAR=UR
+# WSTAR=WE
+# HMX=ALT
+# TS=300.
+# !	ts=tff  !ts(temperatura ambiente),gradt(grad de temperatura)
+# GRADT=0.01
+# HS=AFO
+# ELLE=L
+# DHCONV1=10000.
+# DHCONV2=10000.
+# DHSTAB1=10000.
+# DHSTAB2=10000.
+# !     **************** NEUTRAL  CONDITIONS ****************
+#   A=(1.3*(FB/(US*USTAR**2)))**0.6
+# DHNEUT=HS
+# 10 DHPRV=DHNEUT
+# ZCALC=HS+DHNEUT
+# DHNEUT=A*ZCALC**0.4
+# IF (ABS(DHNEUT-DHPRV).GT.0.01*DHPRV) GOTO 10
+# !     *************** CONVECTIVE CONDITIONS ***************
+#   IF (ELLE.LT.0.) THEN
+# !     ******************* BREAKUP MODEL *******************
+#   DHCONV1=4.3*((FB/(US*WSTAR**2.))**0.6)*HMX**0.4
+# !     ****************** TOUCHDOWN MODEL ******************
+#   B=(FB/(US*0.16*WSTAR**2.)**0.33333)
+# DHCONV2=HS
+# 47 DHCPRV=DHCONV2
+# YCALC=2.*HS+DHCONV2
+# DHCONV2=B*YCALC**0.66666
+# IF(ABS(DHCONV2-DHCPRV).GT.0.01*DHCPRV) GOTO 47
+# IF(HS+DHCONV1.GT.HMX) DHCONV1=10000.
+# IF(HS+DHCONV2.GT.HMX) DHCONV2=10000.
+# !     ****************** STABLE CONDITIONS ****************
+#   ELSE
+# ESSE=(9.81/TS)*(GRADT+0.01)
+# DHSTAB1=2.6*(FB/(US*ESSE))**0.3333
+# DHSTAB2=5.*FB**(1./4.)/ESSE**(3./8.)
+# ENDIF
+# DH=MIN(DHNEUT,DHCONV1,DHCONV2,DHSTAB1,DHSTAB2)
+# QEFF=Q
+# !     ***************** PLUME PENETRATION *****************
+#   IF(DH.GE.(0.62*(HMX-HS))) THEN
+# DHT1=MIN(DHCONV1,DHCONV2,DHNEUT)
+# HMX1=HMX-HS
+# GRADT=0.01
+# ESSE=(9.81/TS)*(GRADT+0.01)
+# DHI=4.*FB**(1./4.)*ESSE**(-3./8.)
+# A1=MAX(0.1,DHI)
+# P=(HMX-HS)/A1
+# IF (P.GE.1.5) PEN=0.
+# IF (P.LE.0.5) PEN=1.
+# IF (P.GT.0.5.AND.P.LT.1.5) PEN=1.5-HMX1/DHI
+# IF (PEN.EQ.1) THEN
+# QEFF=0.1
+# DH=HMX1
+# ELSE IF (PEN.EQ.0)THEN
+# QEFF=Q
+# DH=MIN(DHT1,0.62*HMX1)
+# ELSE IF(PEN.GT.0.AND.PEN.LT.1.)THEN
+# IF(DHT1.GE.(0.62*HMX1))THEN
+# QEFF=Q*(1-PEN)
+# DH=(0.62+0.38*PEN)*(HMX-HS)
+# ELSE
+# QEFF=Q
+# DH=DHT1
+# ENDIF
+# ENDIF
+# ENDIF
+# IF(PEN.NE.0.) THEN
+# ES=Q-QEFF
+# ENDIF
+# AFOE=AFO + DH
+# RETURN
+# END
