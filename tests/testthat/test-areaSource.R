74ad6bbd97ca5bf9f25e71c8e5a5e261c175792a
Schuch666 <underschuch@gmail.com>
2018-06-13 20:31:19 -0300

    more codcov

--- tests/testthat/test-areaSource.R
+++ tests/testthat/test-areaSource.R
@@ -4,5 +4,7 @@ test_that("emissions with source by area", {
   expect_equal(areaSource(raster::shapefile(paste0(system.file("extdata", package = "EmissV"),"/BR.shp"))[22,1],
                           raster::raster(paste0(system.file("extdata", package = "EmissV"),"/dmsp.tiff")),
                           gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d01"))),
-               readRDS(paste0(system.file("tests", package = "EmissV"),"/data01.Rds")))
+               readRDS("data01.Rds"))
 })
+
+# readRDS(paste0(system.file("tests", package = "EmissV"),"/data01.Rds"))
