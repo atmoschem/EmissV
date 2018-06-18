5b5c48691c8842f1bc728d2aa541d01e1824e776
Schuch666 <underschuch@gmail.com>
2018-06-18 02:11:31 -0300

    back to normal

--- tests/testthat/test-lineSource.R
+++ tests/testthat/test-lineSource.R
@@ -1,10 +1,11 @@
 context("lineSource")
 
 test_that("line emissions works", {
-  expect_equal(raster::cellStats(lineSource(read(paste(system.file("extdata",package="EmissV"),"/streets.osm.xz",sep=""),"osm"),
+
+  expect_equal(raster::cellStats(lineSource(readRDS("sf.Rds"),
                                             gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d03")),
                                             as_raster=TRUE),"sum"),
-               sum(lineSource(read(paste(system.file("extdata",package="EmissV"),"/streets.osm.xz",sep=""),"osm"),
+               sum(lineSource(readRDS("sf.Rds"),
                               gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d03")),
                               as_raster=FALSE))
                )
