17c347ceec30f0c36e47db64e6ca119d11026013
Schuch666 <underschuch@gmail.com>
2018-06-18 18:21:16 -0300

    codcov 676

--- tests/testthat/test-lineSource.R
+++ tests/testthat/test-lineSource.R
@@ -4,8 +4,13 @@ test_that("line emissions works", {
     expect_equal(raster::cellStats(lineSource(readRDS("sf.Rds"),
                                             gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d03")),
                                             as_raster=TRUE),"sum"),
+               1
+               )
+
+  expect_equal(1,
                sum(lineSource(readRDS("sf.Rds"),
                               gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d03")),
                               as_raster=FALSE))
-               )
+  )
+
 })
