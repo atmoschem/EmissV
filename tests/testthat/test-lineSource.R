fe14abfd4ee87a362a45e00b0f7a7d9f59137775
Schuch666 <underschuch@gmail.com>
2018-06-18 18:49:44 -0300

    codcov 769

--- tests/testthat/test-lineSource.R
+++ tests/testthat/test-lineSource.R
@@ -5,11 +5,7 @@ test_that("line emissions works", {
   s <- readRDS("sf.Rds")
   g <- gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d03"))
 
-  oldw <- getOption("warn")
-  options(warn = -1)
-
-  # expect_equal(1,sum(lineSource(s,g,as_raster=FALSE)))
+  expect_equal(1,sum(lineSource(s,g,as_raster=FALSE)))
   expect_equal(raster::cellStats(lineSource(s,g,as_raster=TRUE),"sum"),1)
 
-  options(warn = oldw)
 })
