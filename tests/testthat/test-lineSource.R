c64abbb0e0145651ecfc226ce62e52e6f02134d9
Schuch666 <underschuch@gmail.com>
2018-06-18 17:28:33 -0300

    codcov 674

--- tests/testthat/test-lineSource.R
+++ tests/testthat/test-lineSource.R
@@ -1,11 +1,11 @@
-# context("lineSource")
-#
-# test_that("line emissions works", {
-#     expect_equal(raster::cellStats(lineSource(readRDS("sf.Rds"),
-#                                             gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d03")),
-#                                             as_raster=TRUE),"sum"),
-#                sum(lineSource(readRDS("sf.Rds"),
-#                               gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d03")),
-#                               as_raster=FALSE))
-#                )
-# })
+context("lineSource")
+
+test_that("line emissions works", {
+    expect_equal(raster::cellStats(lineSource(readRDS("sf.Rds"),
+                                            gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d03")),
+                                            as_raster=TRUE),"sum"),
+               sum(lineSource(readRDS("sf.Rds"),
+                              gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d03")),
+                              as_raster=FALSE))
+               )
+})
