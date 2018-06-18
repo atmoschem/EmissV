b766a4b1980a49e74af8e157c504158ffc5500b8
Schuch666 <underschuch@gmail.com>
2018-06-18 20:25:06 -0300

    But you learn from what's killing you And this time it's real

--- tests/testthat/test-lineSource.R
+++ tests/testthat/test-lineSource.R
@@ -6,5 +6,6 @@ test_that("line emissions works", {
   g <- gridInfo(paste0(system.file("extdata", package = "EmissV"),"/wrfinput_d03"))
 
   expect_equal(raster::cellStats(lineSource(s,g,as_raster=TRUE),"sum"),1)
+  expect_equal(sum(lineSource(s,g,as_raster=FALSE)),1)
 
 })
